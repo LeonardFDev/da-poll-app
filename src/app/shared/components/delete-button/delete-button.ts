278f6e74a604af5d4ff6018ef9fa07988f48820b
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-03-26 13:12:01 +0100

    feat: delete-button with images

---
+++ src/app/shared/components/delete-button/delete-button.ts
@@ -0,0 +1,11 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-button',
+  imports: [],
+  templateUrl: './delete-button.html',
+  styleUrl: './delete-button.scss',
+})
+export class DeleteButton {
+  
+}
