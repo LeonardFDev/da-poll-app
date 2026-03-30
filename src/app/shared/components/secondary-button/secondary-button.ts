be40744b6c65244705f49988892937f6cad5d258
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-03-30 17:10:48 +0200

    feat: add secondary-button, add image

---
+++ src/app/shared/components/secondary-button/secondary-button.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-button',
+  imports: [],
+  templateUrl: './secondary-button.html',
+  styleUrl: './secondary-button.scss',
+})
+export class SecondaryButton {
+  @Input() btnText: string = '';
+  @Input() withImg: boolean = false;
+  @Input() imgUrl: string = '';
+}
+
+//html template (with img): <app-secondary-button [btnText]="'Add next question'" [withImg]="true" [imgUrl]="'/assets/img/add_circle_secondary_button.png'"></app-secondary-button>
+//html template (without img): <app-secondary-button [btnText]="'Add next question'" [withImg]="false"></app-secondary-button>
