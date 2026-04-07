1bdcf299195f682437151cabd587a2bd0cdd6902
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-04-07 19:47:09 +0200

    fix: continue building the 'view-survey' page. | minor adjustments in create-survey and view-survey (scss)

--- src/app/pages/main/main.ts
+++ src/app/pages/main/main.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { PrimaryButton } from "../../shared/components/primary-button/primary-button";
+import { HeroImage } from "../../shared/components/hero-image/hero-image";
+import { HighlightsCard } from "../../shared/components/highlights-card/highlights-card";
+import { DropDownMenu } from "../../shared/components/drop-down-menu/drop-down-menu";
+import { FilterButton } from "../../shared/components/filter-button/filter-button";
+import { SurveyView } from "../../shared/components/survey-view/survey-view";
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [PrimaryButton, HeroImage, HighlightsCard, DropDownMenu, FilterButton, SurveyView],
   templateUrl: './main.html',
   styleUrl: './main.scss',
 })
