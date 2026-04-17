c4a2175927926eee97e8b0e7ec6abb68a845b8c7
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-04-17 23:46:25 +0200

    fix: minor adjustment survey-question (interface)

--- src/app/shared/interfaces/survey-question.ts
+++ src/app/shared/interfaces/survey-question.ts
@@ -1,11 +1,10 @@
-import { AnswerInterface } from "./answer";
+import { QuestionInterFace } from "./question";
 
 export interface SurveyQuestionInterFace {
     id: number;
     name:string
-    question: string;
-    endDate?: Date;
     description?:string
-    answers: AnswerInterface[];
-    checkboxType: 'checkbox' |'radio';
+    endDate?: Date | number | string;
+    category:string;
+    questions: QuestionInterFace[];
 }
