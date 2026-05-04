8865960479cdc577978ff630eff0ae2d9b3a00cf
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-05-04 19:12:30 +0200

    fix: when in the calendar, the date is deleted there is no empty string, instead 'No end date' is set. In addition, a red box comes around the mandatory fields if they are not filled in.

--- src/app/shared/components/input-field/input-field.ts
+++ src/app/shared/components/input-field/input-field.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, computed, inject, Input, signal } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { QuestionValuesServices } from '../../services/question-values/question-values';
 
@@ -13,4 +13,14 @@ export class InputField {
 
   @Input() type:'inputText' | 'textarea' | 'inputDate'= 'inputText';
   @Input() nameControl!:FormControl;
+
+  get dateInvalid(): boolean {
+    return this.nameControl.invalid && this.nameControl.touched;
+  }
+  
+  calendarValueChange(){
+    const FormControlEndDate = this.qvService.nameControl('endDate');
+    if(!FormControlEndDate.value) FormControlEndDate.setValue('No end date');
+  }
+
 }
