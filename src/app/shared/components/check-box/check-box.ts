9adaf32368094222286c0e56862978d9f097512d
MyCodeLeonard <203679381+MyCodeLeonard@users.noreply.github.com>
2026-05-22 15:21:23 +0200

    fix: minor adjustments

--- src/app/shared/components/check-box/check-box.ts
+++ src/app/shared/components/check-box/check-box.ts
@@ -38,8 +38,7 @@ export class CheckBox {
     this.isChecked.set(!this.isChecked());
     if(this.answerView){
       if(typeof this.multipleAnswers == "object") this.answersFormArray.controls.find(answer => answer?.get('checked')?.setValue(false))
-      this.answerView.setValue(this.inputRef.nativeElement.checked)
-      console.log(this.answersFormArray.value);
+      this.answerView.setValue(this.inputRef.nativeElement.checked);
     }
 
     if(this.nameControl){
